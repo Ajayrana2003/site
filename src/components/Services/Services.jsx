05c7fe21ca17e6baecdba70b07a04501af732dd6
Ajay chauhan <ajay305rana@gmail.com>
2026-05-29 16:16:27 +0530

    added services section and new images

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,51 @@
+import React, { Component, Fragment } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import designIcon from '../../asset/image/design.png';
+import ecommerceIcon from '../../asset/image/ecommerce.png';
+import webIcon from '../../asset/image/web.png';
+import '../../asset/css/custom.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export class Services extends Component {
+  render() {
+    return (
+      <Fragment>
+
+        <Container className='text-center'>
+            <h1 className='serviceMainTitle'>MY SERVICE</h1>
+            <div className='bottom'></div>
+            <Row>
+                <Col lg={4} md={6} sm={12}>
+                
+                <div className='serviceCard text-centre'>
+                    <img className="ecommerceIcon" src={ecommerceIcon}/>
+                    <h1 className='ServiceName' >Ecommerce</h1>
+                    <p className='ServiceDescription'>I will design and develop ecommerce online store website</p>
+                </div>
+                    
+                    </Col>
+                    <Col lg={4} md={6} sm={12}>
+                
+               <div className='serviceCard text-centre'>
+                    <h1 className='ServiceName' >Web Design</h1>
+                    <p className='ServiceDescription'>Qualified web design and attractive effecty which catches visitir eye.</p>
+                </div>
+                    
+                    </Col>
+                    <Col lg={4} md={6} sm={12}>
+                
+               <div className='serviceCard text-centre'>
+                    <img className="webIcon" src={webIcon}/>
+                    <h1 className='ServiceName' >Web Development</h1>
+                    <p className='ServiceDescription'>Clean and fresh air code to make your website dynamic perfectly</p>
+                </div>
+                    
+                    </Col>
+            </Row>
+        </Container>
+      </Fragment>
+    )
+  }
+}
+
+export default Services
